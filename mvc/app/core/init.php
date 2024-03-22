401e63eaf68ac4db4874b6e3bb103bb0b3d55cbc
cucharo <patriziapipperzoe@gamil.com>
2024-03-22 08:54:27 +0800

    all this edit

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -4,5 +4,13 @@
 require 'functions.php';
 require 'App.php';
 require 'Controller.php';
+require 'Model.php';
+require 'Database.php';
+
+spl_autoload_register(functiom ($class_name)) {
+
+    require '../app/models/' . $class_name . '.php';
+
+});
 
 
