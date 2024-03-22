44a1c54ca0c4c8ad76b1fd8918f19a51835b4978
pogi <pogiako@123mail.com>
2024-03-22 09:02:15 +0800

    added

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -3,3 +3,10 @@
 require 'functions.php';
 require 'App.php';
 require 'Controller.php';
+require 'Model.php';
+require 'App.php';
+
+spl_autoload_register(function ($class_name){
+
+    require '../app/models/' . $class_name . '.php';
+});
